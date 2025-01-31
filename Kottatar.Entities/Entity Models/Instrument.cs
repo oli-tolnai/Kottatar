7bbc6421eb9b2bd26af44130aed141c4c1643798
oli.tolnai <oli.tolnai3@stud.uni-obuda.hu>
2025-01-31 16:18:18 +0100

    Music and Instrument done

---
+++ Kottatar.Entities/Entity Models/Instrument.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kottatar.Entities.Entity_Models
+{
+    public class Instrument
+    {
+        public Instrument(string musicId, string type, string auidoFile)
+        {
+            Id = Guid.NewGuid().ToString();
+            MusicId = musicId;
+            Type = type;
+            AuidoFile = auidoFile;
+        }
+
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [StringLength(50)]
+        public string MusicId { get; set; }
+
+        [NotMapped]
+        public virtual Music? Music { get; set; }
+
+        [StringLength(150)]
+        public string Type { get; set; }
+
+        [StringLength(200)]
+        public string AuidoFile { get; set; }
+
+
+    }
+}
---
+++ Kottatar.Entities/Entity Models/Music.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kottatar.Entities.Entity_Models
+{
+    public class Music
+    {
+        public Music(string title, string sheetMusic)
+        {
+            Id = Guid.NewGuid().ToString();
+            Title = title;
+            this.sheetMusic = sheetMusic;
+        }
+
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        [StringLength(250)]
+        public string Title { get; set; }
+
+        [StringLength(200)]
+        public string sheetMusicFile { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Instrument>? Instruments { get; set; }
+
+    }
+}
