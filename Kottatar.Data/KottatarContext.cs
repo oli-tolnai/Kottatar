e43f5ecf6b047801245347b3988a5b5558cc7a8c
oli.tolnai <oli.tolnai3@stud.uni-obuda.hu>
2025-01-31 17:40:47 +0100

    dbcontext done

---
+++ Kottatar.Data/KottatarContext.cs
@@ -0,0 +1,32 @@
+﻿using Kottatar.Entities.Entity_Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kottatar.Data
+{
+    public class KottatarContext : DbContext
+    {
+        public DbSet<Music> Musics { get; set; }
+
+        public DbSet<Instrument> Instruments { get; set; }
+
+        public KottatarContext(DbContextOptions<KottatarContext> ctx)
+            : base(ctx)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Music>()
+                .HasMany(m => m.Instruments)
+                .WithOne(i => i.Music)
+                .HasForeignKey(i => i.MusicId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+
+    }
+}
