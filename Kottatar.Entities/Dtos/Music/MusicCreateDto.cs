74451a2cd4d0ba6fde54497faf090d3cba356c73
oli.tolnai <oli.tolnai3@stud.uni-obuda.hu>
2025-02-01 13:18:58 +0100

    projectlogic created

---
+++ Kottatar.Entities/Dtos/Music/MusicCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kottatar.Entities.Dtos.Music
+{
+    public class MusicCreateDto
+    {
+        public string Title { get; set; } = "";
+        public string SheetMusicFile { get; set; } = "";
+    }
+}
---
+++ Kottatar.Logic/Logic/MusicLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kottatar.Data;
+using Kottatar.Entities.Dtos.Music;
+using Kottatar.Entities.Entity_Models;
+
+
+
+namespace Kottatar.Logic.Logic
+{
+    public class MusicLogic
+    {
+        Repository<Music> repo;
+
+        public MusicLogic(Repository<Music> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void AddMusic(MusicCreateDto dto)
+        {
+            Music m = new Music(dto.Title, dto.SheetMusicFile);
+            repo.Create(m);
+        }
+    }
+}
