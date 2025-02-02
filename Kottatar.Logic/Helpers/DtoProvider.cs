c5c8c17ba247b4a9eff139fa9a0646c244ab4425
oli.tolnai <oli.tolnai3@stud.uni-obuda.hu>
2025-02-02 15:18:02 +0100

    AutoMapper init

---
+++ Kottatar.Logic/Helpers/DtoProvider.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Kottatar.Entities.Dtos.Music;
+using Kottatar.Entities.Entity_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kottatar.Logic.Helpers
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Music, MusicShortViewDto>();
+                cfg.CreateMap<Music, MusicViewDto>();
+                cfg.CreateMap<MusicCreateUpdateDto, Music>();
+
+            });
+
+            Mapper = new Mapper(config);
+        }
+    }
+}
