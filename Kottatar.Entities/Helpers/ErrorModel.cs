d527dd3bc4e3cd42d4ebb91762cdad2f56eba94b
oli.tolnai <oli.tolnai3@stud.uni-obuda.hu>
2025-06-23 21:35:55 +0200

    error model added

---
+++ Kottatar.Entities/Helpers/ErrorModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kottatar.Entities.Helpers
+{
+    public class ErrorModel
+    {
+        public string Message { get; set; }
+        public ErrorModel(string message)
+        {
+            Message = message;
+        }
+        public ErrorModel()
+        {
+            Message = "An error occurred.";
+        }
+    }
+}
